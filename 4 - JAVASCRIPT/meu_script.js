0537f5d5099b6e5ef9eb4b29f9c2dca47d6d67a1
Bruno Viana <brnv1999@gmail.com>
2022-06-25 21:48:46 -0300

    estudosjavascript

---
+++ 4 - JAVASCRIPT/meu_script.js
@@ -0,0 +1,8 @@
+alert("Olá, seja bem vindo(a) ao curso de JavaScript")
+document.getElementById('nome').value = 'oi'
+//comentario de uma linha.
+
+/*comentario de 
+multiplas linhas
+Esses dois tipos funcionam apenas aqui em arquivo js ou css*/
+
